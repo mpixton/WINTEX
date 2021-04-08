82519cf43e1a0d0346944bf40c4d7e862efaac25
mpixton <71086131+mpixton@users.noreply.github.com>
2021-04-08 15:42:25 -0600

    FIX: remove dbsets
    Removed the old dbsets.

--- DAL/FagElGamousDbContext.cs
+++ DAL/FagElGamousDbContext.cs
@@ -18,47 +18,5 @@ namespace WINTEX.DAL
         {
         }
 
-        public virtual DbSet<Bowler> Bowlers { get; set; }
-        public virtual DbSet<Team> Teams { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer("Name=ConnectionStrings:IntexPracticeSqlServer");
-            }
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.HasDefaultSchema("bowlers")
-                .HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
-
-            modelBuilder.Entity<Bowler>(entity =>
-            {
-                entity.Property(e => e.BowlerId).ValueGeneratedNever();
-
-                entity.HasOne(d => d.Team)
-                    .WithMany(p => p.Bowlers)
-                    .HasForeignKey(d => d.TeamId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_Bowlers_TeamID_Teams");
-            });
-
-            modelBuilder.Entity<Team>(entity =>
-            {
-                entity.Property(e => e.TeamId).ValueGeneratedNever();
-
-                entity.HasOne(d => d.Captain)
-                    .WithMany(p => p.Teams)
-                    .HasForeignKey(d => d.CaptainId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_Teams_Captain_Bowlers");
-            });
-
-            OnModelCreatingPartial(modelBuilder);
-        }
-
-        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
