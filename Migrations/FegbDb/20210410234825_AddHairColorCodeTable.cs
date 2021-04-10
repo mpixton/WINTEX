44ba302c1d30ef4a447f0daee8692a2df767493d
mpixton <71086131+mpixton@users.noreply.github.com>
2021-04-10 17:53:42 -0600

    FEAT: add migrations
    Added migrations for the FEGBExcavationContext.

---
+++ Migrations/FegbDb/20210410234825_AddHairColorCodeTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WINTEX.Migrations.FegbDb
+{
+    public partial class AddHairColorCodeTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HairColorCodes",
+                columns: table => new
+                {
+                    HairColorCode = table.Column<string>(type: "text", nullable: false),
+                    HairColorDescription = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HairColorCodes", x => x.HairColorCode);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HairColorCodes");
+        }
+    }
+}
