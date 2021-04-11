9119cf3dfccf32efe4375284e685e7c20ef17ed9
mpixton <71086131+mpixton@users.noreply.github.com>
2021-04-10 22:46:17 -0600

    FEAT: add security and functionality
    Added security features to the researcher controller. Added logging and Unit Of Work. Added actions to create shaft location and tomb location.

--- Controllers/ResearcherController.cs
+++ Controllers/ResearcherController.cs
@@ -1,19 +1,50 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using WINTEX.DAL;
+using WINTEX.Models.ViewModels;
 
 namespace WINTEX.Controllers
 {
+    [Authorize(Roles = "Admin, Researcher")]
+    [ValidateAntiForgeryToken]
     public class ResearcherController : Controller
     {
+        private readonly ILogger _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ResearcherController(ILogger logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+
         // GET: /AddMummy
         [HttpGet]
         public ActionResult AddMummy()
         {
+            _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
+            return View(new AddMummyViewModel());
+        }
+
+        [HttpGet]
+        public ActionResult AddShaftLocation()
+        {
+            _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
+            return View(new AddLocation());
+        }
+
+        [HttpPost]
+        public ActionResult AddShaftLocations(AddLocation newShaft)
+        {
+            _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
             return View();
+        }
+
+        public ActionResult AddTombLocation()
+        {
+            _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
+            return View(new AddTomb());
         }
     }
 }
