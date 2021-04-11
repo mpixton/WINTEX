a9184b8c01805a71f970852f38c7bccf3d870314
mpixton <71086131+mpixton@users.noreply.github.com>
2021-04-10 21:20:54 -0600

    FEAT: view models
    Added a view models to add a Mummy and view Mummies in a list. Added an implicit conversion from Mummy to MummyListView.

---
+++ Models/ViewModels/MummyListView.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WINTEX.Models.ViewModels
+{
+    public class MummyListView
+    {
+        // TODO - Add List View Action
+        // Required attributes - Burial Location, Length, Depth, Burial Num, Sex, Age, Date Excavated, Artifacts, Head Direction, 
+        // Hair, Preservation Index
+        public string BurialLocation { get; set; }
+        public decimal? Length { get; set; }
+        public decimal? BurialDepth { get; set; }
+        public string BurialNum { get; set; }
+        public string Sex { get; set; }
+        public string Age { get; set; }
+        public string DateExcavated { get; set; }
+        public bool? Artifacts { get; set; }
+        public string HeadDirection { get; set; }
+        public string Hair { get; set; }
+        public string PreservationIndex { get; set; }
+
+        public static implicit operator MummyListView(Mummy m)
+        {
+            var dateExcavated = m.DateExcavated.ToString();
+            if (m.DateExcavated is null)
+            {
+                dateExcavated = $"{m.DayExcavated} {m.MonthExcavated} {m.YearExcavated}";
+            }
+            return new MummyListView()
+            {
+                Age = m.AgeCodeSingle,
+                Artifacts = m.ArtifactFound,
+                BurialDepth = m.BurialDepth,
+                BurialLocation = m.Shaft.ToString(),
+                BurialNum = m.BurialNum,
+                Hair = m.HairColorCode,
+                HeadDirection = m.HeadDirection,
+                Length = m.Length,
+                PreservationIndex = m.PreservationIndex,
+                Sex = m.PostExhumationDatum.Sex,
+                DateExcavated = dateExcavated
+            };
+        }
+    }
+}
