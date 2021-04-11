3de0065760347d5d3b5ed5b8f2e82e10c70cc188
mpixton <71086131+mpixton@users.noreply.github.com>
2021-04-10 22:50:01 -0600

    FEAT: update view models
    Renamed and added an implicit conversion operator to the AddMummy view model. Added a Tomb and Shaft location view model.

---
+++ Models/ViewModels/AddMummyViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using WINTEX.Enums;
+
+namespace WINTEX.Models.ViewModels
+{
+    public class AddMummyViewModel //View Model for mummy information/input of mummy burial excavation field data
+    {
+        public int ShaftLocationId { get; set; }
+
+        public int TombLocationId { get; set; }
+
+        [Required]
+        public string BurialNum { get; set; }
+
+        //measurements
+        [Required]
+        public decimal? BurialDepth { get; set; }
+        [Required]
+        public decimal? WestToHead { get; set; }
+        [Required]
+        public decimal? WestToFeet { get; set; }
+        [Required]
+        public decimal? SouthToHead { get; set; }
+        [Required]
+        public decimal? SouthToFeet { get; set; }
+        [Required]
+        public decimal? Length { get; set; }
+
+        //details
+        // TODO - Create static string class for boolean values.
+        [Required]
+        public bool? ArtifactFound { get; set; } //Burial Goods, yes or no
+        [Required]
+        public bool Photo { get; set; }
+        // TODO - Create static string class for preservation index.
+        [Required]
+        public string PreservationIndex { get; set; }
+        [Required]
+        public string ClusterNum { get; set; }
+        // TODO - Create static string class for HairColorCode.
+        [Required]
+        public string HairColorCode { get; set; }
+        // TODO - Create static string class for AgeCodeSingle?
+        [Required]
+        public string AgeCodeSingle { get; set; }
+        [Required]
+        public string BurialMaterials { get; set; }
+        [Required]
+        public string ExcavationRecorder { get; set; }
+
+        //date/time
+        [Required]
+        public DateTime? DateTime { get; set; }
+
+        public static implicit operator Mummy(AddMummyViewModel m)
+        {
+            return new Mummy()
+            {
+                ShaftId = m.ShaftLocationId,
+                TombId = m.TombLocationId,
+                BurialNum = m.BurialNum,
+                ArtifactFound = m.ArtifactFound,
+                BurialDepth = m.BurialDepth,
+                BurialMaterials = m.BurialMaterials,
+                ClusterNum = m.ClusterNum,
+                DateExcavated = m.DateTime,
+                ExcavationRecorder = m.ExcavationRecorder,
+                HairColorCode = m.HairColorCode,
+                Photo = m.Photo,
+                WestToFeet = m.WestToFeet, 
+                WestToHead = m.WestToHead,
+                SouthToFeet = m.SouthToFeet,
+                SouthToHead = m.SouthToHead,
+                PreservationIndex = m.PreservationIndex,
+                Length = m.Length
+            }
+        }
+    }
+}
---
+++ Models/ViewModels/AddTomb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WINTEX.Models.ViewModels
+{
+    public class AddTomb
+    {
+        public string AreaHillBurial { get; set; }
+
+        public string Tomb { get; set; }
+    }
+}
