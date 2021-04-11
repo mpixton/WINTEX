9326a804a26caf95000d131aa6b9ff3819d2e6c0
mpixton <71086131+mpixton@users.noreply.github.com>
2021-04-10 21:20:01 -0600

    FIX: update interface unitofwork
    Updated the IUnitOfWork file so that the UnitOfWork in the controller will work properly.

--- DAL/IUnitOfWork.cs
+++ DAL/IUnitOfWork.cs
@@ -10,7 +10,21 @@ namespace WINTEX.DAL
         /// <summary>
         /// Generic Repo for Bowlers.
         /// </summary>
-        
+        public GenericRepo<BiologicalSample> BiologicalSamples { get; set; }
+        public GenericRepo<BioSamplesNote> BioSampleNotes { get; set; }
+        public GenericRepo<CarbonDating> CarbonDating { get; set; }
+        public GenericRepo<Fegbdatum> FEGBData { get; set; }
+        public GenericRepo<FegbmummyStorage> FEGBMummyStorage { get; set; }
+        public GenericRepo<FegbstorageLocation> FEGBStorageLocations { get; set; }
+        public GenericRepo<Gisdatum> GISData { get; set; }
+        public GenericRepo<Mummy> Mummies { get; set; }
+        public GenericRepo<MummyNote> MummyNotes { get; set; }
+        public GenericRepo<OsteologicalMummyDatum> MummyBoneData { get; set; }
+        public GenericRepo<PostExhumationDatum> PostExhumationData { get; set; }
+        public GenericRepo<ShaftLocation> ShaftLocations { get; set; }
+        public GenericRepo<TombLocation> TombLocations { get; set; }
+        public GenericRepo<HairColorCodes> HairColorCodes { get; set; }
+
 
         /// <summary>
         /// Saves the changes made to the Repos in this Unit Of Work to the Db.
