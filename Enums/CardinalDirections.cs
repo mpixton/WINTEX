7c4fe3d56df2ea574146ce25f189c6d5cc210f9e
mpixton <71086131+mpixton@users.noreply.github.com>
2021-04-10 22:48:13 -0600

    FEAT: add static classes
    Added CardinalDirections and YesNo static classes to handle drop down lists for their areas of responsibility. Removed the Subplots enum as it gets covered in the CardinalDirections static class.

---
+++ Enums/CardinalDirections.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WINTEX.Enums
+{
+    public static class CardinalDirections
+    {
+        [Display(Name = "East")]
+        public static string East = "E";
+
+        [Display(Name = "West")]
+        public static string West = "W";
+
+        [Display(Name = "North")]
+        public static string North = "N";
+
+        [Display(Name = "South")]
+        public static string South = "S";
+
+        [Display(Name = "NorthWest")]
+        public static string NW = "NW";
+
+        [Display(Name = "NorthEast")]
+        public static string NE = "NE";
+        
+        [Display(Name = "SouthWest")]
+        public static string SW = "SW";
+
+        [Display(Name = "SouthEast")]
+        public static string SE = "SE";
+
+        public static IEnumerable<string> EastWest()
+        {
+            return new List<string>()
+            {
+                East, West
+            };
+        }
+
+        public static IEnumerable<string> NorthSouth()
+        {
+            return new List<string>()
+            {
+                North, South
+            };
+        }
+
+        public static IEnumerable<string> SubPlots()
+        {
+            return new List<string>()
+            {
+                NW, NE, SW, SE
+            };
+        }
+    }
+}
