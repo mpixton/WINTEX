03405cb9d534c02450352f6d73b3a2ea36802c98
mpixton <71086131+mpixton@users.noreply.github.com>
2021-04-10 21:22:29 -0600

    FEAT: add list view to controller
    Added the Mummy list view action to the controller. Added logging on all actions.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using WINTEX.DAL;
+using WINTEX.Infrastructure;
 using WINTEX.Models;
+using WINTEX.Models.ViewModels;
 
 namespace WINTEX.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
+            _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
             return View();
         }
 
         public IActionResult Privacy()
         {
+            _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
             return View();
+        }
+
+        public IActionResult ListMummies(int pageNum, int itemsPerPage = 20)
+        {
+            _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
+            _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
+            var mummyList = _unitOfWork.Mummies.GetAll(m => m.PostExhumationDatum, m => m.Shaft).ToList().Cast<MummyListView>();
+            var pageModel = new Paginator<MummyListView>(itemsPerPage, mummyList);
+            return View(pageModel.GetItems(pageNum));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
