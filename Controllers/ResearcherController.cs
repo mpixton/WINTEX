a9db2b8a7220eda8524b6d8c87e4aa5c6ee44f6d
mpixton <71086131+mpixton@users.noreply.github.com>
2021-04-12 13:48:05 -0600

    FEAT: removed duplicate actions
    Removed actions from the Researcher controller that are now handled elsewhere.

--- Controllers/ResearcherController.cs
+++ Controllers/ResearcherController.cs
@@ -21,69 +21,13 @@ namespace WINTEX.Controllers
 
         // GET: /AddMummy
         [HttpGet]
-        public IActionResult AddMummy()
+        public IActionResult AddMummyQuick()
         {
             _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
             return View(new AddMummyViewModel());
         }
 
-        public IActionResult AddMummy(AddMummyViewModel newMummy)
-        {
-            _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult AddShaftLocation()
-        {
-            _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
-            return View(new AddLocation());
-        }
-
-        [HttpPost]
-        public IActionResult AddShaftLocation(AddLocation newShaft)
-        {
-            _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult AddTombLocation()
-        {
-            _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
-            return View(new AddTomb());
-        }
-
-        [HttpPost]
-        public IActionResult AddTombLocation(AddTomb newTomb)
-        {
-            _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult EditMummy()
-        {
-            _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult EditMummy(object editMummy)
-        {
-            _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult DeleteMummy()
-        {
-            _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult DeleteMummy(object deleteMummy)
+        public IActionResult AddMummyQuick(AddMummyViewModel newMummy)
         {
             _logger.LogInformation("{Protocol} {Method} {Path}", Request.Protocol, Request.Method, Request.Path);
             return View();
