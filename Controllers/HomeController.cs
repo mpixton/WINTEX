3417eed1c3c9fa2a70e1bd2138cdf4b0ae3bf2b2
mpixton <71086131+mpixton@users.noreply.github.com>
2021-04-12 09:00:36 -0600

    FIX: build error from namespacing

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -48,6 +48,7 @@ namespace WINTEX.Controllers
         }
 
         // GET: /ListMummies
+
         public IActionResult ListMummies(int pageNum, int itemsPerPage = 20)
         {
             _logger.LogInformation("{Protocol} {Method} {Path} : pageNum {pageNum}, itemsPerPage {itemsPerPage}", Request.Protocol, Request.Method, Request.Path, pageNum, itemsPerPage);
