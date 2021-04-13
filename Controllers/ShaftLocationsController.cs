bc1ba92082d7fcdd4ed68e392f961a92f636bc42
mpixton <71086131+mpixton@users.noreply.github.com>
2021-04-12 23:10:00 -0600

    FEAT: add diagnostic context to each controller

--- Controllers/ShaftLocationsController.cs
+++ Controllers/ShaftLocationsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using WINTEX.DAL;
 using WINTEX.Enums;
 using WINTEX.Infrastructure;
@@ -15,10 +16,12 @@ namespace WINTEX.Controllers
 {
     public class ShaftLocationsController : Controller
     {
+        private readonly IDiagnosticContext diagnosticContext;
         private readonly FEGBExcavationContext _context;
 
-        public ShaftLocationsController(FEGBExcavationContext context)
+        public ShaftLocationsController(IDiagnosticContext diagnosticContext, FEGBExcavationContext context)
         {
+            this.diagnosticContext = diagnosticContext;
             _context = context;
         }
 
