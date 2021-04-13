bd83ef9e350f8832252a59ca5c5ee6df40ab1226
davidschraedel <65734436+davidschraedel@users.noreply.github.com>
2021-04-13 12:07:09 -0600

    somewhat better create mummy page

---
+++ Enums/AgeCodes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WINTEX.Enums
+{
+    public class AgeCodes
+    {
+        [Display(Name = "Adult")]
+        public static string Adult = "A";
+        [Display(Name = "Child")]
+        public static string Child = "C";
+        [Display(Name = "Neonate/Infant")]
+        public static string NeonateInfant = "N/I";
+
+        public static IEnumerable<string> GetValues()
+        {
+            return new List<string>()
+            {
+                Adult, Child, NeonateInfant
+            };
+        }
+    }
+}
